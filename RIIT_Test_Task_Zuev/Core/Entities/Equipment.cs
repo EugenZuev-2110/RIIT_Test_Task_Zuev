c61f0493a2a14ecb159c6d237ac848101f3d4509
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-25 03:18:11 +0300

    Equipment created

---
+++ RIIT_Test_Task_Zuev/Core/Entities/Equipment.cs
@@ -0,0 +1,38 @@
+﻿namespace RIIT_Test_Task_Zuev.Core.Entities;
+
+/// <summary>
+/// Сущность единицы компьютерной техники.
+/// </summary>
+public class Equipment
+{
+    /// <summary>
+    /// Уникальный идентификатор единицы техники.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Учетный номер. Должен быть уникальным и состоять только из цифр и латинских букв. 
+    /// Ограничение: до 32 символов.
+    /// </summary>
+    public string InventoryNumber { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Произвольное наименование техники. Ограничение: до 256 символов.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Идентификатор типа техники (внешний ключ).
+    /// </summary>
+    public int TypeId { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство для связи со справочником типов техники.
+    /// </summary>
+    public virtual EquipmentType EquipmentType { get; set; } = null!;
+
+    /// <summary>
+    /// Номер комнаты размещения оборудования. Ограничение: от 1 до 1000.
+    /// </summary>
+    public int RoomNumber { get; set; }
+}
---
+++ RIIT_Test_Task_Zuev/Core/Entities/EquipmentType.cs
@@ -0,0 +1,22 @@
+﻿namespace RIIT_Test_Task_Zuev.Core.Entities;
+
+/// <summary>
+/// Сущность справочника типов компьютерной техники (например: Монитор, Ноутбук).
+/// </summary>
+public class EquipmentType
+{
+    /// <summary>
+    /// Уникальный идентификатор типа техники.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Наименование типа техники. Ограничение: до 128 символов.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Навигационное свойство. Список техники, относящейся к данному типу.
+    /// </summary>
+    public virtual ICollection<Equipment> Equipments { get; set; } = new List<Equipment>();
+}
