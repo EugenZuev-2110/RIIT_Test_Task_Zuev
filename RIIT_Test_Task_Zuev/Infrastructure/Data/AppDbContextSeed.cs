afd882a2480682144f61c0b4e78505fd8be82010
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-25 04:15:56 +0300

    AppDbContextSeed created

---
+++ RIIT_Test_Task_Zuev/Infrastructure/Data/AppDbContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RIIT_Test_Task_Zuev.Core.Entities;
+
+namespace RIIT_Test_Task_Zuev.Infrastructure.Data
+{
+    /// <summary>
+    /// Класс для автоматического заполнения справочников базы данных при старте приложения.
+    /// </summary>
+    public static class AppDbContextSeed
+    {
+        /// <summary>
+        /// Проверяет справочник типов техники и, если он пуст, наполняет его дефолтными значениями.
+        /// </summary>
+        /// <param name="context">Контекст базы данных Entity Framework.</param>
+        public static async Task SeedAsync(AppDbContext context)
+        {
+            if (!await context.EquipmentTypes.AnyAsync())
+            {
+                var defaultTypes = new[]
+                {
+                    new EquipmentType { Name = "Системный блок" },
+                    new EquipmentType { Name = "Ноутбук" },
+                    new EquipmentType { Name = "Монитор" },
+                    new EquipmentType { Name = "МФУ / Принтер" }
+                };
+
+                await context.EquipmentTypes.AddRangeAsync(defaultTypes);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
