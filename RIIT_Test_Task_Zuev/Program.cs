885bf51430809edd2f49d97534e9a3ff41c7b067
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-25 04:26:28 +0300

    Program modified

--- RIIT_Test_Task_Zuev/Program.cs
+++ RIIT_Test_Task_Zuev/Program.cs
@@ -1,6 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using RIIT_Test_Task_Zuev.Core.Interfaces;
+using RIIT_Test_Task_Zuev.Infrastructure.Data;
+using RIIT_Test_Task_Zuev.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>();
+builder.Services.AddDirectoryBrowser();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        await context.Database.MigrateAsync();
+        await AppDbContextSeed.SeedAsync(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Произошла ошибка при инициализации или миграции базы данных.");
+    }
+}
 
 app.Run();
